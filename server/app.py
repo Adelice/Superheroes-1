65ed87096e9ce30d2b9676e7c34c3440e724e6e4
Nancy Adelice Umutoniwase <nancytoniw@gmail.com>
2024-07-04 16:54:44 +0200

    Superheroes-challenge

---
+++ server/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, make_response,jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import db, Hero, Power, HeroPower
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return '<h1>Code challenge</h1>'
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes= Hero.query.all()
+    serialized_heroes=[hero.to_dict() for hero in heroes]
+    return jsonify(serialized_heroes),200
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero(id):
+    hero=Hero.query.get(id)
+    if not hero:
+        return jsonify({'error':'Hero not found'}),404
+    return jsonify(hero.to_dict()),200
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
